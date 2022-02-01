705aea04a7e3161e52768595cceda39a56f1b55f
wolframalexa <alexajakob@tutanota.com>
2022-01-31 23:25:53 -0500

    fixing callbacks issue

--- main.py
+++ main.py
@@ -517,7 +517,7 @@ if __name__ == "__main__":
                 }
             },
         }
-        callbacks_cfg = lightning_config.callbacks or OmegaConf.create()
+        callbacks_cfg = OmegaConf.create()
         callbacks_cfg = OmegaConf.merge(default_callbacks_cfg, callbacks_cfg)
         trainer_kwargs["callbacks"] = [instantiate_from_config(callbacks_cfg[k]) for k in callbacks_cfg]
 
