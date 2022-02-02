abd15ad74891080d8d19ba51593ac9b9228812ba
wolframalexa <alexajakob@tutanota.com>
2022-02-02 13:45:56 -0500

    cropped images

--- crop.py
+++ crop.py
@@ -1,20 +1,26 @@
 from PIL import Image
 import os
 
-image_directory = "images"
+image_directory = "./images/"
 x = 256
 y = 256
 
-image = "./images/cmp_b0001.jpg"
-#for image in os.listdir(image_directory):
-original = Image.open(image)
+#image = "./images/cmp_b0001.jpg"
+for image in os.listdir(image_directory):
+	try:
+		path = image_directory + image
+		original = Image.open(path)
 
-width, height = original.size   # Get dimensions
-left = (width - x)/2
-top = (height - y)/2
-right = (width + x)/2
-bottom = (height + y)/2
-cropped_example = original.crop((left, top, right, bottom))
-cropped_example.show()
+		width, height = original.size   # Get dimensions
+		left = (width - x)/2
+		top = (height - y)/2
+		right = (width + x)/2
+		bottom = (height + y)/2
+		cropped = original.crop((left, top, right, bottom))
 
-cropped_example.save('./images/cmp_b0001_cropped.jpg', 'JPEG')
+		new_path = './cropped/' + image[:-4] + '_cropped.jpg'
+		print(new_path)
+		cropped.save(new_path, 'JPEG')
+	except:
+		print(image, "is not an image")
+		pass
