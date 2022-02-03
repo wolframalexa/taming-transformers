31ddb5fcacf9ac2a05b2fd6d2a7995f6be2fd54d
wolframalexa <alexajakob@tutanota.com>
2022-02-03 10:30:10 -0500

    separate text/train

--- crop.py
+++ crop.py
@@ -1,11 +1,13 @@
 from PIL import Image
 import os
+import random
+import math
 
 image_directory = "./images/"
 x = 256
 y = 256
 
-#image = "./images/cmp_b0001.jpg"
+# crop facades images to x and y coordinates above
 for image in os.listdir(image_directory):
 	try:
 		path = image_directory + image
@@ -22,5 +24,29 @@ for image in os.listdir(image_directory):
 		print(new_path)
 		cropped.save(new_path, 'JPEG')
 	except:
-		print(image, "is not an image")
+		print(image, "is not an image") # don't crop .txt files
 		pass
+
+
+test = open('./text/facade_test.txt', 'w')
+train = open('./text/facade_training.txt', 'w')
+
+# use a 90-10 split, training-test
+files = os.listdir('./cropped/')
+numfiles = len(files)
+print("The dataset has", numfiles, "files in it")
+
+train_files = random.sample(files, math.floor(numfiles * 0.9)) # remaining files used for test
+test_files = list(set(files) - set(train_files))
+
+for item in train_files:
+	line = './cropped/' + item + '\n'
+	train.writelines(line)
+
+print(len(train_files))
+print(len(test_files))
+
+for item in test_files:
+	line = './cropped/' + item + '\n'
+	test.writelines(line)
+
